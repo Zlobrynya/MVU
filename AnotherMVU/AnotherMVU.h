6a064c2aebe7fbf0975ec46ca7fd07dca0a8bf5d
Zlobrynya <zlobrynya@gmail.com>
2023-10-07 14:41:27 +0300

    Init

---
+++ AnotherMVU/AnotherMVU.h
@@ -0,0 +1,18 @@
+//
+//  AnotherMVU.h
+//  AnotherMVU
+//
+//  Created by Nikitin Nikita Andreevich on 07.10.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AnotherMVU.
+FOUNDATION_EXPORT double AnotherMVUVersionNumber;
+
+//! Project version string for AnotherMVU.
+FOUNDATION_EXPORT const unsigned char AnotherMVUVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnotherMVU/PublicHeader.h>
+
+
